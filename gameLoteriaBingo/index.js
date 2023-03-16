71c345dbcc978014e48f315c5c8943faa8d796fa
25Alan <romeroalan71@gmail.com>
2023-03-15 23:25:44 -0300

    formateo del codigo js

--- gameLoteriaBingo/index.js
+++ gameLoteriaBingo/index.js
@@ -14,12 +14,13 @@ window.onload = () => {
   buttonNumberRandom.addEventListener("click", () => {
     containerInfo.innerHTML = ``;
     buttonNumberRandom.textContent = numberRandom();
-    if(!checkRepeated(buttonNumberRandom.textContent)) numbersGenerated.push(buttonNumberRandom.textContent);
+    if (!checkRepeated(buttonNumberRandom.textContent))
+      numbersGenerated.push(buttonNumberRandom.textContent);
     checkCardboard(buttonNumberRandom.textContent);
-    createCardboard("Check", "Check");
-    for(let i = 0; i < inputNumberPlayers.value; i++){
-      console.log(checkWinner(i+1));
-      if(checkWinner(i) === 15) winner(`Ganador: tabla ${i+1}`);
+    createCardboard("Check", -1, "Check");
+    for (let i = 0; i < inputNumberPlayers.value; i++) {
+      console.log(checkWinner(i + 1));
+      if (checkWinner(i) === 15) winner(`Ganador: tabla ${i + 1}`);
     }
   });
 };
@@ -28,11 +29,10 @@ function createCardboard(check, numberTable, namePlayer = "Player") {
   const table = document.createElement("table");
   const caption = document.createElement("caption");
   const tbody = document.createElement("tbody");
-
-  table.classList.add('table', 'table-bordered', 'border-info', 'text-dark', 'fw-bold');
-  caption.classList.add('bg-transparent', 'text-center');
+  table.classList.add("table","table-bordered","border-info","text-dark","fw-bold");
+  caption.classList.add("bg-transparent", "text-center");
   caption.textContent = namePlayer;
-  tbody.classList.add('text-center');
+  tbody.classList.add("text-center");
 
   function createRowsColumns(rows, cols, value) {
     let count = 0;
@@ -51,7 +51,7 @@ function createCardboard(check, numberTable, namePlayer = "Player") {
     completeNumbers();
     createRowsColumns(3, 5, numbersRandomPlayers);
     numbersRandomPlayers = [];
-    table.setAttribute('id', `table${numberTable}`);
+    table.setAttribute("id", `table${numberTable}`);
     table.appendChild(caption);
     table.appendChild(tbody);
     containerPlayers.appendChild(table);
@@ -63,37 +63,39 @@ function createCardboard(check, numberTable, namePlayer = "Player") {
   }
 }
 
-
 function createXtimes(numbersPlayers = 2) {
   for (let i = 0; i < numbersPlayers; i++) {
-    createCardboard("noCheck",i , `Player ${i + 1}`);
+    createCardboard("noCheck", i, `Player ${i + 1}`);
   }
 }
 
 function checkCardboard(number) {
-  const tdAll = document.querySelectorAll('td');
-  for(let i = 0; i < tdAll.length; i++){
-    if(tdAll[i].textContent === number) tdAll[i].style.backgroundColor = 'rgb(255, 255, 255)';
+  const tdAll = document.querySelectorAll("td");
+  for (let i = 0; i < tdAll.length; i++) {
+    if (tdAll[i].textContent === number)
+      tdAll[i].style.backgroundColor = "rgb(255, 255, 255)";
   }
 }
 
 function checkWinner(numberTable) {
   const tdAll = document.querySelectorAll(`#table${numberTable} td`);
   let countWhite = 0;
-  for(let i = 0; i < tdAll.length; i++) {
-    if(tdAll[i].style.backgroundColor === 'rgb(255, 255, 255)') countWhite++;
+  for (let i = 0; i < tdAll.length; i++) {
+    if (tdAll[i].style.backgroundColor === "rgb(255, 255, 255)") countWhite++;
   }
   return countWhite;
 }
 
 function winner(string) {
-  const winner = document.querySelector('.winner');
-  const h1 = document.querySelector('.winner .box h1');
-  winner.classList.remove('d-none');
+  const winner = document.querySelector(".winner");
+  const h1 = document.querySelector(".winner .box h1");
+  winner.classList.remove("d-none");
   h1.textContent = string;
 }
 
-const numberRandom = () => { return Math.floor(Math.random() * 90) + 1; };
+const numberRandom = () => {
+  return Math.floor(Math.random() * 90) + 1;
+};
 const completeNumbers = () => {
   while (numbersRandomPlayers.length < 15) {
     let random = numberRandom();
